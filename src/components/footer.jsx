da52a8686df94c3ca179c31c382b378b289d62d7
Polina <polinkkaaj@yandex.ru>
2023-06-06 21:56:21 +0300

    g

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer" style={{ backgroundColor: '#8A6FF8' }}>
+        <div className="footer-copyright">
+            <div className="container">
+                © 2023 Copyright Text
+                <a className="grey-text text-lighten-4 right" href="#!">Reprository</a>
+            </div>
+        </div>
+    </footer>
+}
+
+export { Footer };
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav style={{ backgroundColor: '#8A6FF8' }}>
+        <div className="nav-wrapper container" >
+            <a href="#!" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="#!">Reprository</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export { Header };
